66177be117f185dae7b566de2d5317c5c4414049
Rahul Kumar Tiwary <tiwaryrahul311@gmail.com>
2018-11-06 07:49:10 +0530

    Add files via upload

---
+++ QAQ.cpp
@@ -0,0 +1,124 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//defines
+#define openin freopen("input.txt","r",stdin)
+#define openout freopen("output.txt","w",stdout)
+#define fast ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+#define ll long long
+#define int long long
+#define mod 1000000007
+#define repr(i,x,y) for (__typeof(x) i=x;i>=y;i--)
+#define rep(i,x,y) for (__typeof(x) i=x;i<=y;i++)
+#define all(c) (c).begin(),(c).end()
+#define ff first
+#define ss second
+#define pb push_back
+#define mp make_pair
+
+/* Print pair */
+template <typename T,typename S>
+ostream & operator << (ostream &os , const pair<T,S> &v) {
+    os << "(" ;
+    os << v.first << "," << v.second << ")" ;
+    return os ;
+}
+/* Print vector */
+template <typename T>
+ostream & operator << (ostream &os , const vector<T> &v) {
+    os << "[" ;
+    int sz = v.size() ;
+    for(int i = 0 ; i < sz ; ++i) {
+        os << v[i] ;
+        if(i!=sz-1)os << "," ;
+    }
+    os << "]\n" ;
+    return os ;
+}
+/* Print set */
+template <typename T>
+ostream & operator << (ostream &os , const set<T> &v) {
+    T last = *v.rbegin() ;
+    os << "[" ;
+    for(auto it : v) {
+        os << it  ;
+        if(it != last) os << "," ;
+    }
+    os << "]\n" ;
+    return os ;
+}
+/* Print Map */
+template <typename T,typename S>
+ostream & operator << (ostream &os , const map<T,S> &v) {
+    for(auto it : v) {
+        os << it.first << " : " << it.second << "\n" ;
+    }
+    return os ;
+}
+int power(int a , int b)
+{
+    int res = 1 ;
+    while(b)
+    {
+        if(b%2) {
+            res = (res * a) % mod ;
+        }
+        b/=2 ;
+        a = (a*a) % mod ;
+    }
+    return res ;
+}
+
+//debug
+#define TRACE
+
+#ifdef TRACE
+#define trace(...) __f(#__VA_ARGS__, __VA_ARGS__)
+template <typename Arg1>
+void __f(const char* name, Arg1&& arg1){
+        cerr << name << " : " << arg1 << std::endl;
+}
+template <typename Arg1, typename... Args>
+void __f(const char* names, Arg1&& arg1, Args&&... args){
+        const char* comma = strchr(names + 1, ',');cerr.write(names, comma - names) << " : " << arg1<<" | ";__f(comma+1, args...);
+}
+#else
+#define trace(...)
+#endif
+
+const int N = 3e5 + 5 ;
+
+int forwar[N] , backwar[N] ;
+string str;
+pair<pair<int,int>,int> arr[N] ;
+int32_t main() {
+    cin>>str;
+    memset(forwar, 0, sizeof(forwar));
+    memset(backwar, 0, sizeof(backwar));
+    int stringSize = str.size();
+    rep(i, 1, stringSize){
+       if(str[i-1] == 'Q')
+          forwar[i] += forwar[i-1] + 1;
+       else
+          forwar[i] = forwar[i-1];
+    }
+    repr(i, stringSize , 1){
+      if(str[i-1] == 'Q')
+        backwar[i] += backwar[i+1] + 1;
+      else
+       backwar[i] = backwar[i+1];
+    }
+    // rep(i, 1, stringSize){
+    //   cout<<forwar[i]<<" "<<backwar[i]<<endl;
+    // }
+    long long res = 0;
+    rep(i, 1, stringSize){
+      if(str[i-1] == 'A'){
+
+        res += forwar[i] * backwar[i];
+        // cout<<i<<" "<<forwar[i]<<" "<<backwar[i]<<" "<<res<<endl;
+      }
+}
+      cout<<res<<endl;
+      return 0;
+}
